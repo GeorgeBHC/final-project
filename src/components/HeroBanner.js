9a2a980904d8954bd0d298c5817e58f51b2f6fb5
GeorgeBHC <burhucgeo12@gmail.com>
2024-09-04 12:26:34 +0300

    Worked on HeroBanner.js

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,9 +1,61 @@
-import React from 'react'
+import React from "react";
+import { Box, Stack, Typography } from "@mui/material";
 
-const HeroBanner = () => {
-  return (
-    <div>HeroBanner</div>
-  )
-}
+import HeroBannerImage from "../assets/images/banner.png";
 
-export default HeroBanner
+const HeroBanner = () => (
+  <Box
+    sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+    position="relative"
+    p="20px"
+  >
+    <Typography color="#FF2625" fontWeight="600" fontSize="26px">
+      BGN Fitness App
+    </Typography>
+    <Typography
+      fontWeight={700}
+      sx={{ fontSize: { lg: "44px", xs: "40px" } }}
+      mb="23px"
+      mt="30px"
+    >
+      Workout, Smile <br />
+      And Repeat
+    </Typography>
+    <Typography fontSize="20px" fontFamily="Alegreya" lineHeight="35px">
+      Look after the most effective exercises personalized to you
+    </Typography>
+    <Stack>
+      <a
+        href="#exercises"
+        style={{
+          marginTop: "45px",
+          textDecoration: "none",
+          width: "200px",
+          textAlign: "center",
+          background: "#FF2625",
+          padding: "14px",
+          fontSize: "22px",
+          textTransform: "none",
+          color: "white",
+          borderRadius: "4px",
+        }}
+      >
+        Explore Exercises
+      </a>
+    </Stack>
+    <Typography
+      fontWeight={600}
+      color="#FF2625"
+      sx={{
+        opacity: "0.1",
+        display: { lg: "block", xs: "none" },
+        fontSize: "200px",
+      }}
+    >
+      Workout
+    </Typography>
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+  </Box>
+);
+
+export default HeroBanner;
