4137f71efbe31154f0395eed9516ba0b3ffdb242
GeorgeBHC <burhucgeo12@gmail.com>
2024-09-05 20:22:56 +0300

    Worked on ExerciseCard.js

--- src/components/ExerciseCard.js
+++ src/components/ExerciseCard.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Stack, Typography } from '@mui/material';
 
-const ExerciseCard = () => {
+const ExerciseCard = ({ exercise }) => {
   return (
-    <div>ExerciseCard</div>
+    <Link className='exercise-card' to={`/exercise/${exercise.id}`}>
+
+      <img src={exercise.gifUrl} alt={exercise.name} Loading='lazy' />
+      <Stack direction="row">
+          <Button sx={{ml: '21px', color: '#fff', background: '#ffa9a9', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}></Button>
+          <Button sx={{ml: '21px', color: '#fff', background: '#fcc797', fontSize: '14px', borderRadius: '20px', textTransform: 'capitalize'}}></Button>
+
+
+      </Stack>
+    </Link>
   )
 }
 
