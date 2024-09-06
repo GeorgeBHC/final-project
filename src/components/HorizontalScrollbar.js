6845b4ce2d472e5f778eea254e1cd1a070f5d871
GeorgeBHC <burhucgeo12@gmail.com>
2024-09-06 11:36:57 +0300

    Final touch-up on HorizontalScrollbar.js

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,9 +1,9 @@
-import React, {useContext} from 'react'
+import React, { useContext } from 'react';
+import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
 import { Box, Typography } from '@mui/material';
-import { ScrollMenu, VisibilityContext} from 'react-horizontal-scrolling-menu';
 
-import BodyPart from './BodyPart'
-
+import ExerciseCard from './ExerciseCard';
+import BodyPart from './BodyPart';
 import RightArrowIcon from '../assets/icons/right-arrow.png';
 import LeftArrowIcon from '../assets/icons/left-arrow.png';
 
@@ -36,7 +36,7 @@ const HorizontalScrollbar = ({ data, bodyParts, setBodyPart, bodyPart }) => (
         title={item.id || item}
         m="0 40px"
       >
-        <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        {bodyParts ? <BodyPart item={item} setBodyPart={setBodyPart} bodyPart={bodyPart} /> : <ExerciseCard exercise={item} /> }
       </Box>
     ))}
   </ScrollMenu>
