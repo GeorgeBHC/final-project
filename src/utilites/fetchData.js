df5de53fe6b5902ccf47cbf1c1dadf48453e9293
GeorgeBHC <burhucgeo12@gmail.com>
2024-09-06 11:56:44 +0300

    Final touch-up on fetchData.js

--- src/utilites/fetchData.js
+++ src/utilites/fetchData.js
@@ -1,15 +1,22 @@
- export const exerciseOptions = {
- 
-     method: 'GET',
-     headers: {
-       'x-rapidapi-key': '703b69711dmshbbfed8b19c4290ap197d17jsnd3ede6fd641a',
-       'x-rapidapi-host': 'exercisedb.p.rapidapi.com'
-     }
-   };
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+  },
+};
 
- export const fetchData = async (url,options) => {
-    const response = await fetch(url,options);
-    const data = await response.json();
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
 
-    return data;
- }
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
