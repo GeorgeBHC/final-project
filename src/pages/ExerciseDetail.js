2193125bf7720e76cc1d57951e5c1b67079b7d10
GeorgeBHC <burhucgeo12@gmail.com>
2024-09-06 10:29:48 +0300

    Worked on ExerciseDetail.js

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { exerciseOptions, fetchData } from '../utilites/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
+  const [ExerciseDetail, setExerciseDetail] = useState({});
+  const {id} = useParams();
+
+  useEffect(() => {
+    const fetchExercisesData = async () => {
+      const exerciseDbUrl = 'htps://exercisedb.p.rapidapi.com';
+      const youtubeSearchUrl = 'https://youtube-search-and-download.p.rapid.api.com';
+    }
+
+    fetchExercisesData();
+  });
+
   return (
-    <div>ExerciseDetail</div>
+    <Box>
+      <Detail exerciseDetail={} />
+      <ExerciseVideos />
+      <SimilarExercises />
+    </Box>
   )
 }
 
